8a0d3274e7b5a7e22734d3b8e8be090714cc4d4c
Delali Funani <dfunani@live.co.za>
2022-10-14 08:11:34 -0700

    added 5-printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,15 @@
+/**
+* main - prints with proper grammar,
+* but the outcome is a piece of art,
+*
+* Description: prints using printf
+*
+* Return: 0
+*
+*/
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
