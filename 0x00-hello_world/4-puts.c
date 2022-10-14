48e0c346aba8e5f79dd49a1f66f6fea6bdc1c367
Delali Funani <dfunani@live.co.za>
2022-10-13 22:07:07 -0700

    added again

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,10 +2,17 @@
 /**
 *Entry
 *Description: Neww manin to 
+*Exit
 */
 int main(void)
 {
+	/*
+	*Main entry
+	*DescriptionL mew main
+	*exit
+	*/	
 	puts("\"Programming is like building a multilingual puzzle");
 	return 0;
+	//Main exit
 }
 
