a4c414b80fb4ab4fef65bdd4725e4ddf57083259
Delali Funani <dfunani@live.co.za>
2022-10-14 22:56:42 -0700

    added 00 00

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <ctype.h>
+/**
+* main - Entry Point
+*
+* Description: print using outchar
+*
+* Return: 0
+*/
+
+int main(void)
+{
+	int i;
+	int j;
+	int k;
+	int m;
+
+	for (i = 48; i < 58; i++)
+	{
+		for (j = 48; j < 58; j++)
+		{
+			for (k = 48; k < 58; k++)
+			{
+				for (m = 48; m < 58; m++)
+				{
+					if (k == 48 && m == 48)
+					{
+						continue;
+					}
+					else
+					{
+						putchar(i);
+						putchar(j);
+						putchar(' ');
+						putchar(k);
+						putchar(m);
+						putchar(',');
+						putchar(' ');
+					}
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
