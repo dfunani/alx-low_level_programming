e465c96f28cc12dbc7edbe7195b25a7bae3448a8
Delali Funani <dfunani@live.co.za>
2022-10-14 20:42:47 -0700

    added 5 digit

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+* main - entry point
+*
+* Description: eval if pos, neg or zero
+*
+* Return: 0
+*/
+int main(void)
+{
+	int digit;
+
+	for (digit = 0; digit < 10; digit++)
+	{
+		putchar(digit);
+	}
+	putchar('\n');
+	return (0);
+}
