f97d9b26ae9de180c5f8a88ed78aa7a6df9f490c
Delali Funani <dfunani@live.co.za>
2022-10-14 21:06:38 -0700

    added list of numbers with 4

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+* main - entry point
+*
+* Description: eval if pos, neg or zero
+*
+* Return: 0
+*/
+int main(void)
+{
+	int digit;
+
+	for (digit = 48; digit < 58; digit++)
+	{
+		putchar(digit);
+		if (digit != 57)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+		else
+		{
+			putchar('$');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
