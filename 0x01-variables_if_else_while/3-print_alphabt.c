aee6a5453de2500cdaa454745ab99aa7a250fd0c
Delali Funani <dfunani@live.co.za>
2022-10-14 20:21:49 -0700

    added print_alphabt.c without qe

---
+++ 0x01-variables_if_else_while/3-print_alphabt.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+* main - entry point
+*
+* Description: eval if pos, neg or zero
+*
+* Return: 0
+*/
+int main(void)
+{
+	char letter;
+
+	for (letter = 'a'; letter != 'z' + 1; letter++)
+	{
+		if (letter != 'q' && letter != 'e')
+		{
+			putchar(letter);
+		}
+	}
+	putchar('\n');
+	return (0);
+}
