f969e49bced1cb5040cc214206e0dbf1559b3029
Delali Funani <dfunani@live.co.za>
2022-10-14 22:04:48 -0700

    added betty check

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -15,9 +15,9 @@ int main(void)
 	int i;
 	int j;
 
-	for (i = 48; i < 57; i + +)
+	for (i = 48; i < 57; i++)
 	{
-		for (j = i + 1; j < 58; j + +)
+		for (j = i + 1; j < 58; j++)
 		{
 			putchar(i);
 			putchar(j);
