45a44bf1149748e02d7f5ae1d365d9b7c9bfdd54
Delali Funani <dfunani@live.co.za>
2022-10-14 20:52:42 -0700

    added dupliate 5

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+* main - entry point
+*
+* Description: eval if pos, neg or zero
+*
+* Return: 0
+*/
+int main(void)
+{
+	int digit;
+
+	for (digit = 48; digit < 58; digit++)
+	{
+		putchar(digit);
+	}
+	putchar('\n');
+	return (0);
+}
