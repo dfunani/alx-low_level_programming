9a4fdf975e34c5d5f4f19b4ec4c488b7114249c4
Delali Funani <dfunani@live.co.za>
2022-10-18 13:48:49 -0700

    added int

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,12 +1,12 @@
 /**
  * _islower - Returns pos or neg is low or upp
- * @c: The single char to be evaluated
+ * @c: The int to be evaluated
  *
  * Return: 0 or 1
  *
  */
 
-int _islower(char c)
+int _islower(int c)
 {
 	if (c >= 97 && c <= 122)
 	{
