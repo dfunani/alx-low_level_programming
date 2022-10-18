fbe52a0ca72f98625dea7370b3ac3fec83a6f228
Delali Funani <dfunani@live.co.za>
2022-10-18 16:13:42 -0700

    added betty" git push "

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -14,18 +14,22 @@ int print_last_digit(int n)
 	int k;
 	int m;
 
-	if (n < 0)
+	if (n < 0 && n != INT_MIN)
 	{
 		n *= -1;
 	}
 	else if (n == INT_MIN)
 	{
 		n = INT_MAX;
+		k = (n % 10) + 49;
+		m = (n % 10) + 1;
+		write(1, &k, 1);
+		return (m);
 	}
 
 	k = (n % 10) + 48;
 	m = (n % 10);
 
-        write(1, &k, 1);
+	write(1, &k, 1);
 	return (m);
 }
