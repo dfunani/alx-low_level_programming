d9fdf23b677445754daf2e30937d3bf22b370545
Delali Funani <dfunani@live.co.za>
2022-10-18 16:59:04 -0700

    added 24 hours

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -13,8 +13,6 @@ void jack_bauer(void)
 	int h;
 	int M;
 	int m;
-	int S;
-	int s;
 	char q;
 
 	q = ':';
@@ -31,29 +29,18 @@ void jack_bauer(void)
 			{
 				for (m = 0; m < 10; m++)
 				{
-					for (S = 0; S < 6; S++)
-					{
-						for (s = 0; s < 10; s++)
-						{
-							int tH = H + 48;
-							int th = h + 48;
-							int tM = M + 48;
-							int tm = m + 48;
-							int tS = S + 48;
-							int ts = s + 48;
-							char nL = '\n';
+					int tH = H + 48;
+					int th = h + 48;
+					int tM = M + 48;
+					int tm = m + 48;
+					char nL = '\n';
 
-							write(1, &tH, 1);
-							write(1, &th, 1);
-							write(1, &q, 1);
-							write(1, &tM, 1);
-							write(1, &tm, 1);
-							write(1, &q, 1);
-							write(1, &tS, 1);
-							write(1, &ts, 1);
-							write(1, &nL, 1);
-						}
-					}
+					write(1, &tH, 1);
+					write(1, &th, 1);
+					write(1, &q, 1);
+					write(1, &tM, 1);
+					write(1, &tm, 1);
+					write(1, &nL, 1);
 				}
 			}
 		}
