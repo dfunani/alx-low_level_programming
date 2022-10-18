055bd31ad9413677024d399a0c429fbdecf89e07
Delali Funani <dfunani@live.co.za>
2022-10-18 09:15:04 -0700

    added removal of the _putchar f()

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,6 +1,5 @@
 #ifndef MAIN
 #define MAIN
-#include <unistd.h>
 
 /**
  * _putchar - writes the character c to stdout
@@ -9,8 +8,5 @@
  * Return: On success 1.
  * On error, -1 is returned, and errno is set appropriately.
  */
-int _putchar(char c)
-{
-	return (write(1, &c, 1));
-}
+
 #endif
