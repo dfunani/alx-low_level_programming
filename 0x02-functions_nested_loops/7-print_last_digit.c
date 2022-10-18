79690b42a2ae94673c35f9e6e68ce21ee00d44b9
Delali Funani <dfunani@live.co.za>
2022-10-18 15:33:08 -0700

    added %

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * print_last_digit - prints the last digit
+ * @n: number to be eval for the last digit
+ *
+ * Return: int
+ *
+ */
+
+int print_last_digit(int n)
+{
+	return (n % 10);
+}
