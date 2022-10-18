095fa62080369903cb45529248571b2537c7d706
Delali Funani <dfunani@live.co.za>
2022-10-18 15:01:38 -0700

    added r = _isalpha('H');     _putchar(r + '0');     r = _isalpha('o');     _putchar(r + '0');     r = _isalpha(108);     _putchar(r + '0');     r = _isalpha(';');     _putchar(r + '0');     _putchar('\n');     return (0);

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _abs - Returns an absolute version of an input value as in
+ * @n: integer input to be abs
+ *
+ * Return: int > 0
+ *
+ */
+
+int _abs(int n)
+{
+	return (n * -1);
+}
