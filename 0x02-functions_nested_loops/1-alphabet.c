eb34b411f9020d4c983e515c8a3f5c4e8af61c33
Delali Funani <dfunani@live.co.za>
2022-10-18 10:13:46 -0700

    added alphabet lower

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,21 @@
+#include <unistd.h>
+/**
+ *
+ * main - prints alphabet in lowercase
+ *
+ * Return: 0
+ *
+ */
+
+int main(void)
+{
+	char letter;
+
+	for (letter = 'a'; letter != 'z'; letter++)
+	{
+		write(1, &letter, 1);
+	}
+	letter = '\n';
+	write(1, &letter, 1);
+	return (0);
+}
