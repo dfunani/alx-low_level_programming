5591e2996a2631244f9ec18619fb72db9c44c90e
Delali Funani <dfunani@live.co.za>
2022-10-18 13:31:34 -0700

    added print alpha

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,26 @@
+#include <unistd.h>
+
+/**
+ * print_alphabet - prints alphabet in lowercase
+ *
+ * Return: 0
+ *
+ */
+
+int print_alphabet(void)
+{
+	char letter;
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (letter = 'a'; letter != 'z' + 1; letter++)
+		{
+			write(1, &letter, 1);
+		}
+
+		letter = '\n';
+		write(1, &letter, 1);
+	}
+	return (0);
+}
