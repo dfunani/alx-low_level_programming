1e0b7ecc416d18ffc6e3afc9f28bf882a5fa5e92
Delali Funani <dfunani@live.co.za>
2022-10-18 18:28:06 -0700

    added

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,41 @@
+#include "main.h"
+
+/**
+ * print_to_98 - prints the daily clock in 00:00:00 format
+ *
+ * Return: 0
+ *
+ */
+
+void print_to_98(int n)
+{
+	int Tens;
+	int Units;
+	char space;
+	char newLine;
+	char comma;
+	char neg = '-';
+
+	for (Tens = n; Tens < 10; Tens++)
+	{
+		if(Tens > 0)
+		{
+			write(1, &Tens; 1);
+		}
+		else if (Tens < 0)
+		{
+			write(1, &neg, 1);
+			let r = Tens * -1;
+			write(1, &r, 1);
+		}
+
+		for (Units = 0; Units < 10; Units++)
+		{
+			write(1, &Units; 1);
+			space = ' ';
+			comma = ',';
+			write(1, &comma, 1);
+			write
+		}
+	}
+}
