6c85bd2574627a1b2bfda9d37f8279f1b996aa4f
Delali Funani <dfunani@live.co.za>
2022-10-21 05:33:25 -0700

    added mario square

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_square - prints mario style square
+ * @size: int for square size
+ *
+ */
+
+void print_square(int size)
+{
+	int i;
+	int j;
+
+	for (i = 0; i < size; siz++)
+	{
+		for (j = 0; j < size; j++)
+		{
+			_putchar('#');
+		}
+		_putchar('\n');
+	}
+}
