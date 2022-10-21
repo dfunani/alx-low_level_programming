a7fee9045886946273b1290d832fadfb37d1c3ed
Delali Funani <dfunani@live.co.za>
2022-10-21 03:17:52 -0700

    added mul ab

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * mul - multiples two ints
+ * @a: param 1 as an int operand
+ * @b: param 2 as an int operand
+ *
+ * Return: a * b as an int
+ *
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
