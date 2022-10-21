6312b8a731b322c01b4b4b49e3a902e3aa0b9d69
Delali Funani <dfunani@live.co.za>
2022-10-21 09:11:31 -0700

    added fizz buzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -16,21 +16,14 @@ void main(void)
 	{
 		if (i % 3 == 0 && i % 5 == 0)
 		{
-			_putchar('F');
-			_putchar('i');
-			_putchar('z');
-			_putchar('z');
-			_putchar('B');
-			_putchar('u');
-			_putchar('z');
-			_putchar('z');
+			_putchar('F'), _putchar('z'), _putchar('z'),
+				_putchar('B'), _putchar('u'), _putchar('z'),
+				_putchar('z');
 		}
 		else if (i % 3 == 0)
 		{
-			_putchar('F');
-			_putchar('i');
-			_putchar('z');
-			_putchar('z');
+			_putchar('F'), _putchar('i'), _putchar('z'),
+				_putchar('z');
 		}
 		else if (i % 5 == 0)
 		{
@@ -39,7 +32,7 @@ void main(void)
 		}
 		else
 		{
-			r = (i % 10 * 10) + (i % 100 /10), r = (i > 9) ?
+			r = (i % 10 * 10) + (i % 100 / 10), r = (i > 9) ?
 				(i % 10 == 0) ? (i * 10 + 1) :
 				(i % 10 * 10) + (i % 100 / 10) : i;
 			counter = 0;
