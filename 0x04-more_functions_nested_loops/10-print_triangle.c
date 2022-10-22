65f5e0227b04cb77ca4020c2974735700a229c3d
Delali Funani <dfunani@live.co.za>
2022-10-22 13:11:52 -0700

    added triangles function - removed the first spce

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -9,7 +9,7 @@
 
 void print_triangle(int size)
 {
-	int i, int j, int k;
+	int i, j,  k;
 
 	if (size <= 0)
 	{
@@ -17,7 +17,7 @@ void print_triangle(int size)
 	}
 	for (i = 0; i < size; i++)
 	{
-		for (j = 0; j < size - i; j++)
+		for (j = 0; j < size - 1 - i; j++)
 		{
 			_putchar(' ');
 		}
