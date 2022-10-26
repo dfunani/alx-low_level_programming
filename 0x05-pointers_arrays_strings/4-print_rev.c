f19777dc0f6f4fc699080f45bec74a2f1356282a
Delali Funani <dfunani@live.co.za>
2022-10-26 14:21:49 -0700

    added rev using strlen

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * print_rev - reverse a given string
+ * @s: string to be reversed
+ *
+ */
+
+void print_rev(char *s)
+{
+	int i;
+
+	if (!s || !*s)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		i = 0;
+		while (s[i] != '\0')
+		{
+			i++;
+		}
+		while (i >= 0)
+		{
+			_putchar(s[i]);
+			i--;
+		}
+		_putchar('\n');
+	}
+}
