fa125461a033a23d82b469e151596d51bfe316b2
Delali Funani <dfunani@live.co.za>
2022-10-31 11:45:05 -0700

    added strchr

---
+++ 0x07-pointers_arrays_strings/2-strchar.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * _strchr - return a char from a str
+ * @s: string to check
+ * @c: char to find
+ *
+ * Return: pointer to the char identified
+ *
+ */
+
+char *_strchr(char *s, char c)
+{
+	int i;
+	char *p;
+
+	while (s[i])
+	{
+		if (s[i] == c)
+		{
+			*p = &s[i];
+			return p;
+		}
+		i++;
+	}
+}
