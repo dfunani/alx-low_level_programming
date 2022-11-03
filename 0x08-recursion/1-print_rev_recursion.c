1d653b4ed51a9570667323d3ae38f247a93a5aff
Delali Funani <dfunani@live.co.za>
2022-11-03 14:26:34 -0700

    added rev rec fixes

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,20 +1,16 @@
 #include "main.h"
 
 /**
- * _puts_recursion - prints using recursion
+ * _print_rev_recursion - prints using recursion
  * @s: string to print
  *
  */
 
-void _puts_recursion(char *s)
+void _print_rev_recursion(char *s)
 {
 	if (*s)
 	{
-		_puts_recursion(s + 1);
+		_print_rev_recursion(s + 1);
 		_putchar(*s);
-	}
-	else
-	{
-		_putchar('\n');
 	}
 }
