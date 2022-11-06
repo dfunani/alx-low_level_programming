1a608759fc26cebb959cdb3d9cbeb43137d8f8ff
Delali Funani <dfunani@live.co.za>
2022-11-06 03:17:04 -0800

    added print arg count

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - program name is printed
+ * @argc: number of input args
+ * @argv: args to be printed
+ *
+ * Return: int 0 or 1
+ *
+ */
+
+int main(int __attribute__ ((unused)) argc, char __attribute__ ((unused)) **argv)
+{
+	printf("%i\n", argc);
+	return (0);
+}
---
+++ 0x0A-argc_argv/1-args.c~
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - program name is printed
+ * @argc: number of input args
+ * @argv: args to be printed
+ *
+ * Return: int 0 or 1
+ *
+ */
+
+int main(int __attribute__ ((unused)) argc, char **argv)
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
