1481174a4967a687d4df440a786ff172f7025290
Delali Funani <dfunani@live.co.za>
2022-11-08 11:17:13 -0800

    added malloc array - fixed logic - fixed 0

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -14,6 +14,10 @@ char *create_array(unsigned int size, char c)
 	char *array = malloc(sizeof(char) * size);
 	unsigned int i;
 
+	if (size <= 1)
+	{
+		return NULL;
+	}
 	for (i = 0; i < size; i++)
 	{
 		array[i] = c;
