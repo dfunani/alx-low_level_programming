4145c59b1a35a886d22da40700b21908b9a5473e
Delali Funani <dfunani@live.co.za>
2022-11-08 11:50:48 -0800

    added strdup

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * _strdup - array to store the desired cahar
+ * @str: length of array
+ *
+ * Return: pointer to char
+ *
+ */
+
+char *_strdup(char *str)
+{
+	char *array;
+	unsigned int i;
+	unsigned int j;
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		i++;
+	}
+	if (!str || !*str || i == 0)
+	{
+		return (NULL);
+	}
+	array = malloc(sizeof(char) * i);
+	for (j = 0; j <= i; j++)
+	{
+		array[j] = str[j];
+	}
+	return (array);
+}
