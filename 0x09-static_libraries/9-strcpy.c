5ed821272f93e0004317b22b51a489d22ab31c71
Delali Funani <dfunani@live.co.za>
2022-11-09 07:43:41 -0800

    added missing functions

---
+++ 0x09-static_libraries/9-strcpy.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _strncat - empty function
+ * @dest: input 1
+ * @src: input 2
+ * @n: input 3
+ *
+ * Return: 0
+ *
+ */
+
+char *_strncat(char *dest, char *src, int n)
+{
+	return (NULL);
+}
