7bf33b720aabcc21f43541d6c7fa7f513488174c
Delali Funani <dfunani@live.co.za>
2022-11-09 06:00:04 -0800

    added free grid

---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * free_grid - array to store the desired str
+ * @grid: str to concat
+ * @height: str to concat to s1
+ *
+ */
+
+void free_grid(int **grid, int height)
+{
+        for (i = 0; i < height; i++)
+        {
+		free(grid[i]);
+        }
+	return (rows);
+}
