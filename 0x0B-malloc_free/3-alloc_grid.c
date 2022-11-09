f70002472478bed658a5126a830d99d703149517
Delali Funani <dfunani@live.co.za>
2022-11-09 04:16:16 -0800

    added malloc only

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * alloc_grid - array to store the desired str
+ * @width: str to concat
+ * @height: str to concat to s1
+ *
+ * Return: pointer to char
+ *
+ */
+
+int **alloc_grid(int width, int height)
+{
+	int (*arr)[M] = malloc(sizeof(int[N][M]));
+	return (arr);
+}
