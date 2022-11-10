362206e82b001d6080de39168b1bc1771d2644f6
Delali Funani <dfunani@live.co.za>
2022-11-10 11:43:27 -0800

    added realloc

---
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * _realloc - move a block of mem
+ * @ptr: ptr to move
+ * @old_size: size of current ptr
+ * @new_size: size of new ptr
+ *
+ * Return: ptr to new void
+ *
+ */
+
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
+{
+	unsigned int i;
+	void *new_ptr;
+
+	if (ptr == NULL)
+	{
+		return (NULL);
+	}
+	new_ptr = malloc(new_size);
+	for (i = 0; i < old_size; i++)
+	{
+		if (i == new_size)
+		{
+			break;
+		}
+		((char *)new_ptr)[i] = ((char *)ptr)[i];
+	}
+	free (ptr);
+	return (new_ptr);
+}
