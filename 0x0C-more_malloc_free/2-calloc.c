9c284ebbf5ef62ac5a335d48c6885ef6044a9cfc
Delali Funani <dfunani@live.co.za>
2022-11-10 12:13:05 -0800

    added test

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -12,15 +12,20 @@
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
 	void *ptr;
+	unsigned int i;
 
 	if (nmemb == 0 || size == 0)
 	{
 		return (NULL);
 	}
-	ptr = malloc(size * nmemb);
-	if (ptr == 0 || ptr == NULL)
+	ptr = malloc(sizeof(size) * nmemb);
+	if ( ptr == NULL)
 	{
 		return (NULL);
+	}
+	for (i = 0; i < nmemb; i++)
+	{
+		((int *)ptr)[i] = 0;
 	}
 	return (ptr);
 }
