f19c9f9a8f62d2c4556ebc183544fbaa4928a560
Delali Funani <dfunani@live.co.za>
2022-11-15 07:53:41 -0800

    added init dog

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,17 @@
+#include "dog.h"
+
+/**
+ * init_dog - function to create an instance of a dog
+ * @d: ptr to the Dog Class/Struct
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: owner of the dog
+ *
+ */
+
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	d -> name = name;
+	d -> age = age;
+	d -> owner = owner;
+}
