642a738ea73c55d272d815f5766a6848d930f8c8
Delali Funani <dfunani@live.co.za>
2022-11-15 07:45:13 -0800

    added the dig struct - sep typedef - README- betty

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,5 +1,16 @@
 #ifndef _DOG_
 #define _DOG_
+
+/**
+ * struct dog - dog obj
+ * @name: name of dog obj
+ * @age: age of dog
+ * @owner: dog owner
+ *
+ * Description: An object to template creating a dog
+ *
+ */
+
 struct dog
 {
 	char *name;
