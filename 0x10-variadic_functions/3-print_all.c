e7e4416f935e97d6423e5f4b24dd11d4d93840be
Delali Funani <dfunani@live.co.za>
2022-11-17 13:34:32 -0800

    added betty - final betty

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -13,9 +13,7 @@ void print_all(const char * const format, ...)
 	unsigned int i;
 
 	while (format == NULL)
-	{
 		return;
-	}
 	va_start(int_list, format);
 	i = 0;
 	while (format[i] != '\0')
@@ -25,15 +23,12 @@ void print_all(const char * const format, ...)
 		case 'c':
 			printf("%c", va_arg(int_list, int));
 			break;
-
 		case 'i':
 			printf("%i", va_arg(int_list, int));
 			break;
-
 		case 'f':
 			printf("%f", va_arg(int_list, double));
 			break;
-
 		case 's':
 			s = va_arg(int_list, char *);
 			if (s == NULL)
@@ -46,11 +41,8 @@ void print_all(const char * const format, ...)
 		}
 		if ((format[i] == 'c' || format[i] == 'i' || format[i] == 'f' ||
 		     format[i] == 's') && format[i + 1] != '\0')
-		{
 			printf(", ");
-		}
 		i++;
 	}
-	va_end(int_list);
-	printf("\n");
+	va_end(int_list), printf("\n");
 }
