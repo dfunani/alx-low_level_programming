117f3011eb3baf224a1f382e9e9883c637f5f446
Delali Funani <dfunani@live.co.za>
2022-11-17 13:15:33 -0800

    added print all

---
+++ 0x10-variadic_functions/3-print_all.c
@@ -0,0 +1,47 @@
+#include "variadic_functions.h"
+
+/**
+ * print_all - prints nums sep by char
+ * @format: sep the ints
+ *
+ */
+
+void print_all(const char * const format, ...)
+{
+	va_list int_list;
+	char *s;
+	unsigned int i;
+
+	va_start(int_list, format);
+	i = 0;
+	while (format[i] != '\0')
+	{
+		switch (format[i])
+		{
+		case 'c':
+			printf("%c", va_arg(int_list, int));
+			break;
+
+		case 'i':
+			printf("%i", va_arg(int_list, int));
+			break;
+
+		case 'f':
+			printf("%f", va_arg(int_list, double));
+			break;
+
+		case 's':
+			s = va_arg(int_list, char *);
+			s == NULL ? printf("(nil)") : printf("%s", s);
+			break;
+		}
+		if ((format[i] == 'c' || format[i] == 'i' || format[i] == 'f' ||
+		     format[i] == 's') && format[i + 1] != '\0')
+		{
+			printf(", ");
+		}
+		i++;
+	}
+	va_end(int_list);
+	printf("\n");
+}
