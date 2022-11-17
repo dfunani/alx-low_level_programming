e41369c3f337a7521041234be9b8b7d2ec9aafa2
Delali Funani <dfunani@live.co.za>
2022-11-17 10:30:03 -0800

    added calc - betty - fixed newline on error

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -16,19 +16,19 @@ int main(int argc, char **argv)
 
 	if (argc != 4)
 	{
-		printf("%s", "Error\n");
+		printf("%s\n", "Error");
 		return (98);
 	}
 	if ((strcmp(argv[2], "/") == 0 || strcmp(argv[2], "%") == 0)
 	    && strcmp(argv[3], "0"))
 	{
-		printf("%s", "Error\n");
+		printf("%s\n", "Error");
 		return (100);
 	}
 	f = get_op_func(argv[2]);
 	if (f == NULL)
 	{
-		printf("%s", "Error\n");
+		printf("%s\n", "Error");
 		return (99);
 	}
 	printf("%i\n", (f(atoi(argv[1]), atoi(argv[3]))));
