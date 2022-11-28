e92c87a29d73076373a6276724da833431091c93
Delali Funani <dfunani@live.co.za>
2022-11-28 11:42:51 -0800

    added valgrind

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+
+/**
+ * free_listint2 - list to fre
+ * @head: list head
+ *
+ */
+
+void free_listint2(listint_t **head)
+{
+	listint_t *trav;
+
+	if ((*head) == NULL)
+	{
+		*head = NULL;
+	}
+	while ((*head) != NULL)
+	{
+		trav = *head;
+		*head = (*head)->next;
+		free(trav);
+	}
+}
