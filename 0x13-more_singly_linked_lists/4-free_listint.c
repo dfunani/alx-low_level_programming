780287eeeca38a3f36e99aa6d9a65c679046dcf3
Delali Funani <dfunani@live.co.za>
2022-11-28 11:31:00 -0800

    added free list

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+/**
+ * free_listint - list to fre
+ * @head: list head
+ *
+ */
+
+void free_listint(listint_t *head)
+{
+	listint_t *trav;
+	if (head == NULL)
+	{
+		return;
+	}
+	while (head != NULL)
+	{
+		trav = head;
+		head = head->next;
+		free(trav);
+	}
+}
