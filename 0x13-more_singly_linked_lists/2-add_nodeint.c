889ed749c77e7b0732f20d33b507fdda8cfab19e
Delali Funani <dfunani@live.co.za>
2022-11-28 10:41:45 -0800

    added printnode added - fixed null

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -14,6 +14,8 @@ listint_t *add_nodeint(listint_t **head, const int n)
 	listint_t *ptr;
 
 	ptr = (listint_t *)malloc(sizeof(listint_t));
+	if (ptr == NULL)
+		return (NULL);
 	ptr->n = n;
 	ptr->next = (*head);
 	(*head) = ptr;
