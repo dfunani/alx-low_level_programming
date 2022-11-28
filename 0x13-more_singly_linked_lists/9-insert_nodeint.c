8c4e765534f917e00f87d48e065af06a783d2d00
Delali Funani <dfunani@live.co.za>
2022-11-28 14:48:43 -0800

    added print - fixed null

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -16,7 +16,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	listint_t *cpy, *node = malloc(sizeof(listint_t));
 
 	i = 0;
-	if ((*head) == NULL || node == NULL)
+	if (head == NULL || node == NULL)
 	{
 		return (NULL);
 	}
