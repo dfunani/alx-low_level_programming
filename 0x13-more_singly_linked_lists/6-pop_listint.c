630ecc3ddfb94af0fe06e40b6696b5a117ed8a4e
Delali Funani <dfunani@live.co.za>
2022-11-28 12:25:18 -0800

    added pop head

---
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+
+/**
+ * pop_listint - remove head from lit
+ * @head: list
+ *
+ * Return: data int
+ *
+ */
+
+int pop_listint(listint_t **head)
+{
+	int trav;
+	listint_t *temp;
+
+	if ((*head) == NULL)
+		return (0);
+	trav = (*head)->n;
+	temp = (*head);
+	(*head) = (*head)->next;
+	free(temp);
+	return trav;
+}
