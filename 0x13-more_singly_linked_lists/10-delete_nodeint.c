edd995e308fd1df610ad63b813df4bddd5f77d4c
Delali Funani <dfunani@live.co.za>
2022-11-29 09:32:08 -0800

    added delete node

---
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -0,0 +1,40 @@
+#include "lists.h"
+
+/**
+ * delete_nodeint_at_index - delete node
+ * @head: list
+ * @index: num
+ *
+ * Return: int
+ *
+ */
+
+int delete_nodeint_at_index(listint_t **head, unsigned int index)
+{
+	listint_t *temp, *cpy;
+	unsigned int i;
+
+	i = 0;
+	cpy = (*head);
+	if (cpy == NULL)
+		return (-1);
+	if (index == 0)
+	{
+		(*head) = cpy->next;
+		free(cpy);
+		return(1);
+	}
+	while (cpy != NULL)
+	{
+		if (index == i + 1)
+		{
+			temp = cpy->next;
+			cpy->next = temp->next;
+			free(temp);
+			return (1);
+		}
+		cpy = cpy->next;
+		i++;
+	}
+	return (-1);
+}
