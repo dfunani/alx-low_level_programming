4e6cce7a67f59741b58bc8545e1e3b0ef9d20da8
Delali Funani <dfunani@live.co.za>
2022-12-02 19:38:42 -0800

    added Right shift without really understanding, please refer to video

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * get_bit - print int binary
+ * @n: long
+ * @index: point
+ *
+ * Return: int
+ *
+ */
+
+int get_bit(unsigned long int n, unsigned int index)
+{
+	return ((n >> index) & 1);
+}
