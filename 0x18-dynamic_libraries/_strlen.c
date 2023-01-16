4667c3cdd0ae0ccbeccc113ee932063bddaa03d6
Delali Funani <dfunani@live.co.za>
2023-01-16 09:42:30 -0800

    added funcs

--- 0x18-dynamic_libraries/_strlen.c
+++ 0x18-dynamic_libraries/_strlen.c
@@ -1,30 +1,30 @@
 int _strlen(char *s)
 {
-	return;
+	return (0);
 }
 int _putchar(char c)
 {
-	return;
+	return (0);
 }
 int _islower(int c)
 {
-	return;
+	return (0);
 }
 int _isalpha(int c)
 {
-	return;
+	return (0);
 }
 int _abs(int n)
 {
-	return;
+	return (0);
 }
 int _isupper(int c)
 {
-	return;
+	return (0);
 }
 int _isdigit(int c)
 {
-	return;
+	return (0);
 }
 void _puts(char *s)
 {
@@ -32,49 +32,49 @@ void _puts(char *s)
 }
 char *_strcpy(char *dest, char *src)
 {
-	return;
+	return (0);
 }
 int _atoi(char *s)
 {
-	return;
+	return (0);
 }
 char *_strcat(char *dest, char *src)
 {
-	return;
+	return (0);
 }
 char *_strncat(char *dest, char *src, int n)
 {
-	return;
+	return (0);
 }
 char *_strncpy(char *dest, char *src, int n)
 {
-	return;
+	return (0);
 }
 int _strcmp(char *s1, char *s2)
 {
-	return;
+	return (0);
 }
 char *_memset(char *s, char b, unsigned int n)
 {
-	return;
+	return (0);
 }
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
-	return;
+	return (0);
 }
 char *_strchr(char *s, char c)
 {
-	return;
+	return (0);
 }
 unsigned int _strspn(char *s, char *accept)
 {
-	return;
+	return (0);
 }
 char *_strpbrk(char *s, char *accept)
 {
-	return;
+	return (0);
 }
 char *_strstr(char *haystack, char *needle)
 {
-	return;
+	return (0);
 }
