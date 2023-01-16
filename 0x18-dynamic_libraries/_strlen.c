18e480ebc210641ead6a9848da10e4153b785a20
Delali Funani <dfunani@live.co.za>
2023-01-16 09:34:21 -0800

    addded so pro

--- 0x18-dynamic_libraries/_strlen.c
+++ 0x18-dynamic_libraries/_strlen.c
@@ -1,4 +1,80 @@
 int _strlen(char *s)
 {
-	return 0;
+	return;
+}
+int _putchar(char c)
+{
+	return;
+}
+int _islower(int c)
+{
+	return;
+}
+int _isalpha(int c)
+{
+	return;
+}
+int _abs(int n)
+{
+	return;
+}
+int _isupper(int c)
+{
+	return;
+}
+int _isdigit(int c)
+{
+	return;
+}
+void _puts(char *s)
+{
+	return;
+}
+char *_strcpy(char *dest, char *src)
+{
+	return;
+}
+int _atoi(char *s)
+{
+	return;
+}
+char *_strcat(char *dest, char *src)
+{
+	return;
+}
+char *_strncat(char *dest, char *src, int n)
+{
+	return;
+}
+char *_strncpy(char *dest, char *src, int n)
+{
+	return;
+}
+int _strcmp(char *s1, char *s2)
+{
+	return;
+}
+char *_memset(char *s, char b, unsigned int n)
+{
+	return;
+}
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	return;
+}
+char *_strchr(char *s, char c)
+{
+	return;
+}
+unsigned int _strspn(char *s, char *accept)
+{
+	return;
+}
+char *_strpbrk(char *s, char *accept)
+{
+	return;
+}
+char *_strstr(char *haystack, char *needle)
+{
+	return;
 }
