27e8cb454e24925da85048ff7a113202ed0965d8
Delali Funani <dfunani@live.co.za>
2023-02-04 00:14:28 -0800

    added hash fixes

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -13,13 +13,14 @@ hash_table_t *hash_table_create(unsigned long int size)
 	hash_table_t* table;
 	long unsigned int i;
 
+	if (size <= 0)
+		return (NULL);
 	table = (hash_table_t*)malloc(sizeof(hash_table_t));
 	if (!table)
 		return (NULL);
 	table->size = size;
-	table->array = (hash_node_t**)calloc((*table).size,
-					      sizeof(hash_node_t));
-	if (!table->array || (*table->array) == NULL)
+	table->array = (hash_node_t**)malloc(sizeof(hash_node_t) * size);
+	if (!table->array)
 		return (NULL);
 	for(i = 0; i < table->size; i++)
 	{
