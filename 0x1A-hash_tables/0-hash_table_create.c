97c7ac6a3f4a579dc3396dec9db8e4959f9b0326
Delali Funani <dfunani@live.co.za>
2023-02-04 01:05:58 -0800

    added final

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -10,25 +10,23 @@
 
 hash_table_t *hash_table_create(unsigned long int size)
 {
-	hash_table_t *new_table;
-	unsigned long int index;
+	hash_table_t *table;
+	unsigned long int i;
 
-	if (size <= 0) /* size must be a positive number */
+	if (size <= 0)
 		return (NULL);
 
-/* allocate memory for table and array and check for fail */
-	new_table = malloc(sizeof(hash_table_t));
-	if (new_table == NULL)
+	table = malloc(sizeof(hash_table_t));
+	if (table == NULL)
 		return (NULL);
-	new_table->array = malloc(sizeof(hash_node_t *) * size);
-	if (new_table->array == NULL)
+	table->array = malloc(sizeof(hash_node_t *) * size);
+	if (table->array == NULL)
 	{
-		free(new_table);
+		free(table);
 		return (NULL);
 	}
-	new_table->size = size;
-/* traverse size and set each array index to null inside table */
-	for (index = 0; index < size; index++)
-		new_table->array[index] = NULL;
-	return (new_table);
+	table->size = size;
+	for (i = 0; i < size; i++)
+		table->array[i] = NULL;
+	return (table);
 }
