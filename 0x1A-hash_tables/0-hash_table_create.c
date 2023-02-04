3852b3022a8a0dd0bd801a6451ec9ae015974cbf
Delali Funani <dfunani@live.co.za>
2023-02-04 00:20:56 -0800

    added hash fixes - betty

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -10,19 +10,19 @@
 
 hash_table_t *hash_table_create(unsigned long int size)
 {
-	hash_table_t* table;
-	long unsigned int i;
+	hash_table_t *table;
+	unsigned long int i;
 
 	if (size <= 0)
 		return (NULL);
-	table = (hash_table_t*)malloc(sizeof(hash_table_t));
+	table = malloc(sizeof(hash_table_t));
 	if (!table)
 		return (NULL);
 	table->size = size;
-	table->array = (hash_node_t**)malloc(sizeof(hash_node_t) * size);
+	table->array = malloc(sizeof(hash_node_t) * size);
 	if (!table->array)
 		return (NULL);
-	for(i = 0; i < table->size; i++)
+	for (i = 0; i < table->size; i++)
 	{
 		(*table).array[i] = NULL;
 	}
